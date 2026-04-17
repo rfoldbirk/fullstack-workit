30f751f6420fb5ec365fe93bff55da4b455d1d99
= <rasmusfoldberg@gmail.com>
2026-04-17 22:52:10 +0200

    stopper med at leake passwords haha

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -4,7 +4,7 @@ import { prisma } from '../prisma'
 const router = Router()
 
 router.get('/', async (req, res) => {
-  const users = await prisma.users.findMany()
+  const users = await prisma.users.findMany({ select: { name: true } })
   res.json(users)
 })
 export default router
